95418e993fe2f93a3803e7376bbab9d5f6142582
potent-silence <75715464+potent-silence@users.noreply.github.com>
2023-05-19 15:32:30 +0530

    BFS.cpp
    Question link:
    https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
    Video Link:
    https://www.youtube.com/watch?v=-tgVpUgsQ5k&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=5
    
    Read Comments to understand code better

---
+++ Graphs/1_BFS.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans,visited(V,0);// Creating ans and visited array to mark the visited nodes
+        queue<int>indexes;// Creating a queue to store indexes while traversing level wise
+        indexes.push(0);
+        while(!indexes.empty()){
+            // Code here is similar to BFS traversal in trees
+            int n=indexes.size();// Taking initial size of queue
+            while(n--){
+                int f=indexes.front();
+                indexes.pop();
+                if(visited[f])  continue;// If node is traversed continue to next node
+                ans.push_back(f);
+                visited[f]=1;// Mark the visited node
+                for(int i=0;i<adj[f].size();i++)    indexes.push(adj[f][i]);// Adding nodes in the queue that are not traversed
+            }
+        }
+        return ans;
+    }
+};
