6b0bcb8794c8908f957f5e1d69b739cbd86b4d47
potent-silence <75715464+potent-silence@users.noreply.github.com>
2023-05-21 22:27:51 +0530

    2D Dp diagonally in Matrix
    Question Link:
    https://leetcode.com/problems/01-matrix/
    
    Similar Question is solved using Dynamic Programming on GFG (Distance of nearest cell having 1):
    Question Link:
    https://practice.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
    Github Solution Link:
    https://github.com/potent-silence/DSA-Practice/blob/main/Graphs/010.%20Distance%20of%20nearest%20cell%20having%201.cpp

---
+++ Dynamic_Programming/010. 01 Matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size(),high=m+n;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(mat[i][j]==0)    continue;
+                // Here we are moving down and right (i.e in diagonally down right direction) and sees if there is 0 we increment it using previous data
+                int d=high,r=high;
+                if(i+1<n)  d=mat[i+1][j];
+                if(j+1<m)  r=mat[i][j+1];
+                mat[i][j]=min(d,r)+1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)    continue;
+                // Here we are moving top and left (i.e in diagonally top left direction) and sees if there is 0 and we assign it with min value possible
+                int t=high,l=high;
+                if(i-1>=0)  t=mat[i-1][j];
+                if(j-1>=0)  l=mat[i][j-1];
+                mat[i][j]=min(mat[i][j],min(t,l)+1);// Here we assign it minimum between previous and now
+            }
+        }
+        return mat;
+    }
+};
