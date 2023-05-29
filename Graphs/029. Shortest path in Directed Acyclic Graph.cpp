dbeee70e83fa67e54fad09d46bea01bd89fb91bf
potent-silence <75715464+potent-silence@users.noreply.github.com>
2023-05-29 19:39:08 +0530

    Using BFS Topo for shortest path
    Question Link:
    https://practice.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
    
    Read Comments for Better Understanding.

---
+++ Graphs/029. Shortest path in Directed Acyclic Graph.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+     vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
+        // code here
+        vector<int>ans(n,INT_MAX),degree(n,0);
+        vector<vector<pair<int,int>>>adj(n);
+        for(int i=0;i<m;i++){
+            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            if(degree[edges[i][0]]>0 || edges[i][0]==0) degree[edges[i][1]]++;// We are incremented the degree of a node only if it is coming from node 0
+        }
+        
+        ans[0]=0;
+        queue<int>ind;
+        ind.push(0);
+        while(!ind.empty()){
+            // Code here is similar to BFS traversal in trees
+            int f=ind.front();
+            ind.pop();
+            
+            for(auto it:adj[f]){
+                degree[it.first]--; // Reducing the Indegree at every time we are traversing it
+                if(degree[it.first]==0) // If the Indegree becomes 0
+                    ind.push(it.first); // We push it in the queue
+                
+                ans[it.first]=min(ans[it.first],ans[f]+it.second);// Comparing the shortest path possible, even if degree exists is 0 or not 
+            }
+	   }
+	   
+	   for(int i=1;i<ans.size();i++)
+	        if(ans[i]==INT_MAX)
+	            ans[i]=-1; // The nodes which have INT_MAX as answer are the ones where we cant reach
+	   
+	   return ans;
+    }
+};
